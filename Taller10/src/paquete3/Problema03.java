d300ba217b4114107aa5fd46c70a9aa8739fa9c3
carloskes <cscorrea@utpl.edu.ec>
2024-12-19 12:51:22 -0500

    actualizado

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,5 +10,45 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
-    
+
+    public static void main(String[] args) {
+
+        String[][] notasCualitativas = new String[3][3];
+        double[][] notasCuantitativas = {{7.1, 8, 10}, {3, 7, 9}, {10, 9, 2.1}};
+        String[] valorNotasCualitativas = new String[3];
+        int suma = 0;
+
+        for (int i = 0; i < notasCualitativas.length; i++) {
+            for (int j = 0; i < notasCuantitativas.length; i++) {
+                if (notasCuantitativas[i][j] >= 0 && notasCuantitativas[i][j]
+                        < 3) {
+                    valorNotasCualitativas[i] = "Insuficiente";
+                } else {
+                    if (notasCuantitativas[i][j] >= 3 && notasCuantitativas[i][j] < 5) {
+                        valorNotasCualitativas[i] = "Regular";
+                    } else {
+                        if (notasCuantitativas[i][j] >= 5 && notasCuantitativas[i][j] < 8) {
+                            valorNotasCualitativas[i] = "Buena";
+                        } else {
+                            if (notasCuantitativas[i][j] >= 8
+                                    && notasCuantitativas[i][j] < 9.5) {
+                                valorNotasCualitativas[i] = "Muy buena";
+                            } else {
+                                if (notasCuantitativas[i][j] >= 9.6
+                                        && notasCuantitativas[i][j] <= 10) {
+                                    valorNotasCualitativas[i] = "Sobresaliente";
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+            
+            
+            
+            
+
+        }
+    }
 }
